5e520a9588e35566cce827cb671cbb060cb5df85
rAel-Ael <alediru9267@gmail.com>
2026-05-06 17:56:30 +0200

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -11,34 +11,56 @@ const ListaFilm = [
 ];
 
 function App() {
-  const [film, setFilm] = useState(ListaFilm);
+const [film] = useState(ListaFilm);
   const [selectedGenere, setSelectedGenere] = useState('');
-  const [filteredFilm, setFilteredFilm] = useState(film);
+  const [filteredFilm, setFilteredFilm] = useState(ListaFilm);
+
+useEffect(() => {
+  let filtroFilm;
+
+  if (selectedGenere === '') {
+    filtroFilm = film;
+  } else {
+    filtroFilm = film.filter(movie => movie.genere === selectedGenere);
+  }
+
+  setFilteredFilm(filtroFilm);
+
+}, [selectedGenere, film]);
+  
+
   return (
     <>
       <h2>Lista film</h2>
       <div>
         <p>Filtra per genere</p>
-      <select
-        value={selectedGenere}
-        onChange={(e) => setSelectedGenere(e.target.value)}  //salva il valore/genere scelto, l'utente cambia il genere e viene salvato
-      >
-        <option value="">Tutti</option>
-        <option value="Fantascienza">Fantascienza</option>
-        <option value="Thriller">Thriller</option>
-        <option value="Romantico">Romantico</option>
-        <option value="Azione">Azione</option>
-      </select>
-  </div>
+        <select
+          value={selectedGenere}
+          onChange={(e) => setSelectedGenere(e.target.value)}  //salva il valore/genere scelto, l'utente cambia il genere e viene salvato
+        >
+          <option value="">Tutti</option>
+          <option value="Fantascienza">Fantascienza</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Romantico">Romantico</option>
+          <option value="Azione">Azione</option>
+        </select>
+      </div>
       <ul>
-        {filteredFilm.map((movie, id) => (
-          <li key={id}>
+        {filteredFilm.map((movie) => (
+          <li key={movie.id}>
             {movie.title} - {movie.genere}
           </li>
         ))}
       </ul>
     </>
   );
+
+
+
+
+
+
+
 }
 
 export default App;
